d04934bb9caee19a84be94665fc9437624932330
Yuri Barros <148558227+Yuribrros@users.noreply.github.com>
2024-04-07 22:33:26 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,160 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/churrasco/pod2.jpg",
+            "name": "Melancia ice",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 90
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/churrasco/pod4.jpg",
+            "name": "Morango/banana",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 90
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/cardapio/churrasco/pod3.jpg",
+            "name": "morango kiwi",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 90
+        },
+       
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/cardapio/burguers/pod21.jpg",
+            "name": "Maça verde",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 120
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "./img/cardapio/burguers/pod21.jpg",
+            "name": "Menta",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 120
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "./img/cardapio/burguers/pod21.jpg",
+            "name": "uva",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 120
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Menta ice",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 120
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Maça verde",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 120
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Morango/kiwi",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 120
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/pod8.jpg",
+            "name": "Maça/uva",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 120
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Morango ice",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 120
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "./img/cardapio/burguers/pod10.jfif",
+            "name": "Uva",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 60
+        },
+        
+    ],
+    
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/pod18.jfif",
+            "name": "Melancia Morango",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 110
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/pod17.jfif",
+            "name": "Menta refrescante",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 110
+        },
+       
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/pod21.jpg",
+            "name": "Uva ice",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 120
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/bebidas/pod21.jpg",
+            "name": "Maça verde",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 120
+        },
+        
+    ],
+    "pizzas": [
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Maçã verde",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 120
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Menta ice",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 120
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Morango ice",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 120
+        },
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/pod8.jpg",
+            "name": "Maça uva",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 120
+        },
+        
+    ]
+}
